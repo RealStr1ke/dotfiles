d7314647d1c03a630bb0a4d6cd39586bede30397
Thandi M <TMenelas@outlook.com>
2022-10-28 23:29:40 -0400

    Tweaks to install.sh
    Former-commit-id: 87af40dbd7a920ab927f29e752d66deb678beb01

--- install.sh
+++ install.sh
@@ -64,9 +64,21 @@ for DOTFILE in `find $HOME/.dotfiles/runcom -type f -name ".*" -printf "%f\n"`
 do
     echo "Creating symlink to $DOTFILE (runcom) in home directory."
 	if [ -f "$HOME/$DOTFILE" ]; then
-		[[ -d "$HOME/.dotfiles/backup" ]] || mkdir -p "$HOME/.dotfiles/backup"
+		[[ -d "$HOME/.dotfiles/backup/runcom" ]] || mkdir -p "$HOME/.dotfiles/backup/runcom"
 		echo "$DOTFILE already exists in the home directory, creating backup at $HOME/.dotfiles/backup/$DOTFILE"
 		cp "$HOME/$DOTFILE" "$HOME/.dotfiles/backup/$DOTFILE"
+	fi
+	[ -f "$HOME/.dotfiles/runcom/$DOTFILE" ] && ln -sf "$HOME/.dotfiles/runcom/$DOTFILE" "$HOME/$DOTFILE"
+done
+
+# Configuration files
+for CONFIG in `find $HOME/.dotfiles/config -name ".*" -printf "%f\n"`
+do
+    echo "Creating symlink to $CONFIG (runcom) in home directory."
+	if [ -f "$HOME/$CONFIG" ]; then
+		[[ -d "$HOME/.dotfiles/backup/config" ]] || mkdir -p "$HOME/.dotfiles/backup/config"
+		echo "$CONFIG already exists in the home directory, creating backup at $HOME/.dotfiles/backup/$CONFIG"
+		cp -r "$HOME/$CONFIG" "$HOME/.dotfiles/backup/$CONFIG"
 	fi
 	[ -f "$HOME/.dotfiles/runcom/$DOTFILE" ] && ln -sf "$HOME/.dotfiles/runcom/$DOTFILE" "$HOME/$DOTFILE"
 done
