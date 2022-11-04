89dbb1fa6a84279ae0f1c190c70ee3b7fcf2300c
Thandi M <TMenelas@outlook.com>
2022-11-04 14:51:17 -0400

    Added new env var to launch.sh
    Former-commit-id: 9b0bff1a71625d4479b926e751603c32f7e26fc7

--- config/waybar/launch.sh
+++ config/waybar/launch.sh
@@ -6,5 +6,8 @@ killall -q waybar
 # Make sure GDK_BACKEND is set to wayland
 export GDK_BACKEND=wayland
 
+# Make sure XDG_CURRENT_DESKTOP is set to Unity (idk waybar said it was needed for a functional tray)
+export XDG_CURRENT_DESKTOP=Unity
+
 # Launch waybar with logs being piped to /tmp/waybar.log
 waybar -l info -c ~/.config/waybar/config > /tmp/waybar.log 2>&1 & disown
