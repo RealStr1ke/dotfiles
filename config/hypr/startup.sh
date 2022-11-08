c9cae1e9b3d226963076a55f39617bfc7a572341
Thandi M <TMenelas@outlook.com>
2022-11-08 09:31:20 -0500

    Tweaks to Hyprland startup script

--- config/hypr/startup.sh
+++ config/hypr/startup.sh
@@ -1,8 +1,28 @@
+# Clipboard Manager (copyq)
 copyq &
+
+# Network Manager Applet 
 nm-applet &
+
+# Discord
 discord &
+
+# Notification Daemon
 dunst &
+
+# Spotify Daemon
 spotifyd &
+
+# Mail Client (mailspring)
 mailspring &
+
+# Waybar
 ~/.config/waybar/launch.sh &
+
+# Policy Authentication Agent 
+# /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 lxpolkit &
+
+# Bluetooth Utilities
+# /usr/lib/bluetooth/mpris-proxy &
+blueman-applet &
