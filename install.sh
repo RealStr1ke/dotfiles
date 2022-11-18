594639f1597cb5f8375b75ce389c917a69eac77f
Thandi M <TMenelas@outlook.com>
2022-11-18 10:56:10 -0500

    Fixed install scripts for dotfiles and packages
    Former-commit-id: ffe6a0c177cc2f9c8ac9fe0702cc4de338ae2180
    Former-commit-id: 2238df15999ea03edb5d1991e40be92678c505a9

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ fi
 # Installation
 echo "Installing .files from RealStr1ke/dotfiles..."
 
-# Runcom files
+# Symlink runcom files
 for DOTFILE in `find $HOME/.dotfiles/runcom -type f -name ".*" -printf "%f\n"`
 do
     echo "Creating symlink to $DOTFILE (runcom) in home directory."
@@ -82,6 +82,18 @@ do
 		rm -r "$HOME/.config/$CONFIG"
 	fi
 	[ -d "$HOME/.dotfiles/config/$CONFIG" ] && ln -sf "$HOME/.dotfiles/config/$CONFIG" "$HOME/.config/$CONFIG"
+done
+
+# Symlink .config files
+for CONFIG in `find $HOME/.dotfiles/config -type f -printf "%f\n"`
+do
+	echo "Creating symlink to $CONFIG (config) in home directory."
+	if [ -f "$HOME/.config/$CONFIG" ]; then
+		[[ -d "$HOME/.dotfiles/backup/config" ]] || mkdir -p "$HOME/.dotfiles/backup/config"
+		echo "$CONFIG already exists in the home directory, creating backup at $HOME/.dotfiles/backup/$CONFIG"
+		cp "$HOME/.config/$CONFIG" "$HOME/.dotfiles/backup/$CONFIG"
+	fi
+	[ -f "$HOME/.dotfiles/config/$CONFIG" ] && ln -sf "$HOME/.dotfiles/config/$CONFIG" "$HOME/.config/$CONFIG"
 done
 
 # Homebrew Installation
--- packages/install.sh
+++ packages/install.sh
@@ -2,34 +2,119 @@ echo "→ Installing packages..."
 
 cd "$DOTFILES/packages"
 
+# Ask for sudo password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Homebrew
-echo "→ Installing Homebrew taps, formulae, and casks..."
-brew bundle
+# TODO: Make different Brewfiles for macOS and Linux
+if ! is-executable brew; then
+	echo "Homebrew is not installed."
+	while true; do
+		read -p "Do you wish to install Homebrew? (y/n) " yn
+		case $yn in
+			[Yy]* ) echo "Installing Homebrew..."; /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"; break;;
+			[Nn]* ) echo "Skipping Homebrew installation."; break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+fi
+
+if is-executable brew; then
+	echo "Homebrew is installed."
+	while true; do
+		read -p "Do you wish to install Homebrew packages? (y/n) " yn
+		case $yn in
+			[Yy]* ) echo "Installing Homebrew tabs, formulae, and casks..."; brew bundle; break;;
+			[Nn]* ) echo "Skipping Homebrew package installation."; break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+fi
 
 # NPM
-echo "→ Installing NPM packages globally..."
-input="$DOTFILES/packages/npmfile"
-while IFS= read -r line
-do
-	sudo npm install -g $line
-done < "$input"
+# Prompt for NPM installation
+if ! is-executable npm; then
+	echo "NPM is not installed."
+	while true; do
+		read -p "Do you wish to install NPM? (y/n) " yn
+		case $yn in
+			[Yy]* ) echo "Installing NPM..."; curl -L https://npmjs.org/install.sh | sh; break;;
+			[Nn]* ) echo "Skipping NPM installation."; break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+fi
+# Prompt for NPM packages installation. If user agrees, install NPM packages from npmfile, assuming that each line contains a package name (use sed to replace \n with space).
+if is-executable npm; then
+	echo "NPM is installed."
+	while true; do
+		read -p "Do you wish to install NPM packages? (y/n) " yn
+		case $yn in
+			[Yy]* ) echo "Installing NPM packages..."; npm install -g $(sed ':a;N;$!ba;s/\n/ /g' npmfile); break;;
+			[Nn]* ) echo "Skipping NPM package installation."; break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+fi
+
 
 # Visual Studio Code
-echo "→ Installing VSCode extensions..."
-input="$DOTFILES/packages/Codefile"
-while IFS= read -r line
-do
-	code --install-extension $line
-done < "$input"
+
+# echo "→ Installing VSCode extensions..."
+# input="$DOTFILES/packages/Codefile"
+# while IFS= read -r line
+# do
+# 	code --install-extension $line
+# done < "$input"
 
 # Pip (Python)
-echo "→ Installing PIP packages..."
-input="$DOTFILES/packages/pipfile"
-while IFS= read -r line
-do
-	pip install $line
-done < "$input"
+# Prompt for PIP package installation. If user agrees, install PIP packages from pipfile, assuming that each line contains a package name (use sed to replace \n with space).
+# If PIP is not installed, skip this step.
+if is-executable pip; then
+	echo "PIP is installed."sed ':a;N;$!ba;s/\n/ /g' pacfile
+	while true; do
+		read -p "Do you wish to install PIP packages? (y/n) " yn
+		case $yn in
+			[Yy]* ) echo "Installing PIP packages..."; pip install $(sed ':a;N;$!ba;s/\n/ /g' pipfile); break;;
+			[Nn]* ) echo "Skipping PIP package installation."; break;;
+			* ) echo "Please answer yes or no.";;
+		esac
+	done
+fi
 
+# Arch Linux packages
+if [[ "$OS" == "arch" ]]; then
+	echo "Arch Linux has been detected (bts I use Arch)."
+	if ! is-executable paru; then
+		echo "Paru is not installed."
+		while true; do
+			read -p "Do you wish to install Paru? (y/n) " yn
+			case $yn in
+				[Yy]* ) echo "Installing Paru..."; mkdir -p ~/.local/src; cd ~/.local/src; git clone https://aur.archlinux.org/paru.git; cd paru; makepkg -si; cd "$DOTFILES/packages"; break;;
+				[Nn]* ) echo "Skipping Paru installation."; break;;
+				* ) echo "Please answer yes or no.";;
+			esac
+		done
+	fi
+	if is-executable paru; then
+		echo "Paru is installed."
+		while true; do
+			read -p "Do you wish to install Arch Linux packages? (y/n) " yn
+			case $yn in
+				[Yy]* ) echo "Installing Arch Linux packages..."; paru -S $(sed ':a;N;$!ba;s/\n/ /g' pacfile); break;;
+				[Nn]* ) echo "Skipping Arch Linux package installation."; break;;
+				* ) echo "Please answer yes or no.";;
+			esac
+		done
+	fi
+fi
+
+
+
+# Unset input variable
 unset input
 
 echo "→ Installation complete! All packages have been installed."
