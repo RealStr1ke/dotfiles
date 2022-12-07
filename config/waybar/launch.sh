0159500cda548630218f9fbae013e4dbe32ab713
Thandi M <TMenelas@outlook.com>
2022-12-07 01:00:53 -0500

    XDG + Hyprland + waybar going at it again
    Former-commit-id: b7c5cff243bcd8e50805f2443cf2d0018cff6af0

--- config/waybar/launch.sh
+++ config/waybar/launch.sh
@@ -10,7 +10,10 @@ export GDK_BACKEND=wayland
 # export XDG_CURRENT_DESKTOP=Unity
 
 # Make sure XDG_CURRENT_DESKTOP is set to sway (I'm gonna need this more because of flameshot)
-export XDG_CURRENT_DESKTOP=sway
+# export XDG_CURRENT_DESKTOP=sway
+
+# I guess I'm ignoring that last XDG_CURRENT_DESKTOP thing because it's not working
+export XDG_CURRENT_DESKTOP=Hyprland
 
 # Launch waybar with logs being piped to /tmp/waybar.log
 waybar -l info -c ~/.config/waybar/config > /tmp/waybar.log 2>&1 & disown
