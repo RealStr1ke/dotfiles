b36b811319dbec750365c08a15fecadea9c9b4b1
Thandi M <TMenelas@outlook.com>
2022-12-10 23:41:08 -0500

    Completed NeoVim config for now
    Former-commit-id: cafa0a1a2e95efdb2c6e988c2475a3cac675e61f

--- config/nvim/lua/configs/alpha.lua
+++ config/nvim/lua/configs/alpha.lua
@@ -22,7 +22,7 @@ dashboard.section.buttons.val = {
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "chrisatmachine.com"
+  return "\n\"Documentation? We don't do that here\" - @Eoghanmc22"
 end
 
 dashboard.section.footer.val = footer()
