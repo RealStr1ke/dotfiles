6462e1f36ba25ed23f646869e42a435afccacadc
Thandi M. <TMenelas@outlook.com>
2023-09-13 10:38:57 -0400

    Another random Neovim to-do list addition

--- bootstrap/os/linux/config/nvim/init.lua
+++ bootstrap/os/linux/config/nvim/init.lua
@@ -4,9 +4,7 @@
 --   > folke/neoconf.nvim
 --   > rebelot/kanagawa.nvim
 --   > sidebar-nvim/sidebar.nvim
---   > folke/noice.nvim
 --   > folke/which-key.nvim
---   > nvim-neo-tree/neo-tree.nvim
 --   > folke/todo-comments.nvim
 --   > CRAG666/code_runner.nvim
 --   > famiu/bufdelete.nvim
@@ -14,6 +12,7 @@
 --   > rmagatti/auto-session
 --   > Yggdroot/LeaderF
 --   > tpope/commentary.vim
+--   > jose-elias-alvarez/null-ls.nvim
 -- > 
 
 -- Start the Main Loader
