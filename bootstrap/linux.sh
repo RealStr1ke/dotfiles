2ac41403e6dc34f2c7f5f612105fc112bb312fd8
Thandi M. <TMenelas@outlook.com>
2023-10-06 14:18:20 -0400

    Updated install scripts (They should work now)

--- bootstrap/linux.sh
+++ bootstrap/linux.sh
@@ -1,28 +1,143 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
+if [ -z "${BASH_VERSION:-}" ]; then
+    abort "Bash is required to interpret this script."
+fi
+
+# Variables
+SOURCE="https://github.com/RealStr1ke/dotfiles"
+TARGET="$HOME/.dotfiles"
+
+# Functions
+function is-executable() {
+    type "$1" > /dev/null 2>&1
+}
+function is::gitpod() {
+    # Check for existent of this gitpod-specific file and the ENV var.
+    # Courtesy to axonasif @ github
+    test -e /ide/bin/gitpod-code && test -v GITPOD_REPO_ROOT;
+}
+
+
+# ==========================
+# | Installation procedure |
+# ==========================
 
 # If there is the "headless" argument, set the variable to true.
 if [ "$1" == "headless" ]; then
     headless=true
+# Else if: This is a Gitpod environment.
+elif is::gitpod; then
+    headless=true
+fi
+
+# If either git or gum (non-headless) is not installed, abort.
+if [ ! is-executable "git" ]; then
+    echo "Git is required to install the dotfiles."
+    exit
+elif [ ! is-executable "gum" &&  ! "$headless" == true ]; then
+    echo "Gum is required to install the dotfiles."
+    exit
 fi
 
 # Get parameters by user input using gum
 # ./bin/gum choose full minimal --header "Pick the type of installation for the dotfiles" --header.foreground 213
 # ./bin/gum choose packages fonts themes wallpapers --header "Pick the extra stuff to install (space to select, enter to continue)" --header.foreground 213 --no-limit
-if [ "$headless" == true ]; then
-    installType="minimal"
-else
-    installType=$(./bin/gum choose full minimal --header "Pick the type of installation for the dotfiles" --header.foreground 213)
-    if [ "$installType" == "full" ]; then
-        extra=$(./bin/gum choose packages fonts themes wallpapers --header "Pick the extra stuff to install (space to select, enter to continue)" --header.foreground 213 --no-limit)
+if [ ! "$headless" == true ]; then
+    if [ -f /etc/arch-release ]; then
+        echo "=====> Distro: Arch Linux"
+        if ! is-executable "git"; then
+            echo "======> Installing git (you might need your sudo password)..."
+            sudo pacman -S git
+        elif ! is-executable "gum"; then
+            echo "======> Installing gum (you might need your sudo password)..."
+            sudo pacman -S gum
+        fi
     fi
-    # Parse extra parameter with new line as delimiter
-    IFS=$'\n' extra=($extra)
 fi
 
 # Install dotfiles
 
+# Clone the repository to $TARGET
+# git clone $SOURCE $TARGET
+# cd $TARGET
+
+# ============> Symlinking
+
+# Runcom
+for RUNCOM in `find $TARGET/bootstrap/os/linux/runcom -type f -name ".*" -printf "%f\n"`
+do
+    if [ -f "$HOME/$RUNCOM" ]; then
+        [[ -d "$TARGET/backup/linux/runcom" ]] || mkdir -p "$TARGET/backup/linux/runcom"
+        if [ "$1" == "headless" ]; then
+            cp -r "$HOME/$RUNCOM" "$TARGET/backup/linux/runcom"
+        else
+            # Prompt with gum if the user wants to backup the file using gum confirm
+            gum confirm "Runcom: Do you want to backup $RUNCOM?"
+            if [ $? -eq 0 ]; then
+                cp -r "$HOME/$RUNCOM" "$TARGET/backup/linux/runcom"
+            fi
+        fi
+    fi
+
+    # Symlink the runcom file
+    [ -f "$TARGET/bootstrap/os/linux/runcom/$RUNCOM" ] && ln -sf "$TARGET/bootstrap/os/linux/runcom/$RUNCOM" "$HOME/$RUNCOM"
+done
+
+# Config Directories
+for CONFIG in `find $TARGET/bootstrap/os/linux/config -type d -printf "%f\n"`
+do
+    if [ -d "$HOME/.config/$CONFIG" ]; then
+        [[ -d "$TARGET/backup/linux/config" ]] || mkdir -p "$TARGET/backup/linux/config"
+        if [ "$1" == "headless" ]; then
+            cp -r "$HOME/.config/$CONFIG" "$TARGET/backup/linux/config"
+        else
+            # Prompt with gum if the user wants to backup the file using gum confirm
+            gum confirm "Config: Do you want to backup $CONFIG?"
+            if [ $? -eq 0 ]; then
+                cp -r "$HOME/.config/$CONFIG" "$TARGET/backup/linux/config"
+            fi
+        fi
+    fi
+done
+
+# Config Files
+for CONFIG in `find $TARGET/bootstrap/os/linux/config -type f -name ".*" -printf "%f\n"`
+do
+    if [ -f "$HOME/$CONFIG" ]; then
+        [[ -d "$TARGET/backup/linux/config" ]] || mkdir -p "$TARGET/backup/linux/config"
+        if [ "$1" == "headless" ]; then
+            cp -r "$HOME/$CONFIG" "$TARGET/backup/linux/config"
+        else
+            # Prompt with gum if the user wants to backup the file using gum confirm
+            gum confirm "Config: Do you want to backup $CONFIG?"
+            if [ $? -eq 0 ]; then
+                cp -r "$HOME/$CONFIG" "$TARGET/backup/linux/config"
+            fi
+        fi
+    fi
+
+    # Symlink the config file
+    [ -f "$TARGET/bootstrap/os/linux/config/$CONFIG" ] && ln -sf "$TARGET/bootstrap/os/linux/config/$CONFIG" "$HOME/$CONFIG"
+done
 
 
-# TEST: Print parameters
-# echo "type: $installType"
-# echo "extra: ${extra[@]}"
+# ============> Extras
+if [ ! headless == true ]; then
+    # Prompt user if they want to install the wallpapers (via the submodule)
+    gum confirm "Extras: Do you want to install the wallpapers?"
+    if [ $? -eq 0 ]; then
+        echo "============> Updating the wallpapers submodule..."
+        # Wallpapers submodule: RealStr1ke/Wallpapers at bootstrap/os/common/wallpapers
+        git submodule update --init --recursive bootstrap/os/common/wallpapers
+        echo "============> Wallpapers submodule updated!"
+    fi
+fi
+
+# Dotfiles installation complete!
+echo "============> Installation complete!"
+echo "→ https://github.com/RealStr1ke/dotfiles"
+echo "→ Thank you for using RealStr1ke's dotfiles!"
+echo "→ Please restart your system to see the changes."
+echo "→ Enjoy!"
+echo "=====================================>"
--- install.sh
+++ install.sh
@@ -1,110 +1,25 @@
 #!/usr/bin/env bash
 # bash -c "`curl -fsSL https://raw.githubusercontent.com/RealStr1ke/dotfiles/master/install.sh`"
 
-if [ -z "${BASH_VERSION:-}" ]; then
-	abort "Bash is required to interpret this script."
-fi
-
-# Importing
-SOURCE="https://github.com/RealStr1ke/dotfiles"
-TARBALL="$SOURCE/tarball/master"
-TARGET="$HOME/.dotfiles"
-TAR_CMD="tar -xzv -C "$TARGET" --strip-components=1 --exclude='{.gitignore}'"
-
-function is-executable() {
-	type "$1" > /dev/null 2>&1
-}
-
-function is::gitpod() {
-    # Check for existent of this gitpod-specific file and the ENV var.
-    # Courtesy to axonasif @ github
-	test -e /ide/bin/gitpod-code && test -v GITPOD_REPO_ROOT;
-}
-
-PATH="$HOME/.dotfiles/bin:$PATH"
-mkdir -p "$HOME/.local/bin"
-
-if is-executable "git"; then
-	echo "Using git for installation"
-	CMD="git clone $SOURCE $TARGET"
-elif is-executable "curl"; then
-	echo "Using curl for installation"
-	CMD="curl -#L $TARBALL | $TAR_CMD"
-elif is-executable "wget"; then
-	echo "Using wget for installation"
-	CMD="wget --no-check-certificate -O - $TARBALL | $TAR_CMD"
-fi
-
-if [ -z "$CMD" ]; then
-	echo "No git, curl or wget available. Aborting."
-	exit
+# If Linux is detected, run bootstrap/linux.sh
+# If macOS is detected, tell the user that macOS is not supported yet.
+# If Windows is detected, tell the user that Windows is not supported yet.
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	echo "=====> Detected OS: Linux"
+	bash -c "`curl -fsSL https://raw.githubusercontent.com/RealStr1ke/dotfiles/master/bootstrap/linux.sh`"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "=====> Detected OS: macOS"
+	echo "=====> macOS is not supported yet."
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+	echo "=====> Detected OS: Windows"
+	echo "=====> Windows is not supported yet."
+elif [[ "$OSTYPE" == "msys" ]]; then
+	echo "=====> Detected OS: Windows"
+	echo "=====> Windows is not supported yet."
+elif [[ "$OSTYPE" == "win32" ]]; then
+	echo "=====> Detected OS: Windows"
+	echo "=====> Windows is not supported yet."
 else
-	if ! is::gitpod; then
-		while true; do
-			read -p "Do you wish to install the dotfiles with the given provider? (y/n) " yn
-			case $yn in
-				[Yy]* ) echo "Installing..."; break;;
-				[Nn]* ) echo "Aborting."; exit;;
-				* ) echo "Please answer yes or no.";;
-			esac
-		done
-	fi
-	mkdir -p "$TARGET"
-	eval "$CMD"
-	echo "Downloaded .files from RealStr1ke/dotfiles!"
-	touch "$HOME/.dotfiles/shell/global/.extra"
-	cd "$TARGET"
+	echo "=====> Detected OS: Unknown"
+	echo "=====> Unknown OS is not supported yet."
 fi
-
-# Installation
-echo "Installing .files from RealStr1ke/dotfiles..."
-
-# Symlink runcom files
-for DOTFILE in `find $HOME/.dotfiles/runcom -type f -name ".*" -printf "%f\n"`
-do
-    echo "Creating symlink to $DOTFILE (runcom) in home directory."
-	if [ -f "$HOME/$DOTFILE" ]; then
-		[[ -d "$HOME/.dotfiles/backup/runcom" ]] || mkdir -p "$HOME/.dotfiles/backup/runcom"
-		echo "$DOTFILE already exists in the home directory, creating backup at $HOME/.dotfiles/backup/$DOTFILE"
-		cp "$HOME/$DOTFILE" "$HOME/.dotfiles/backup/$DOTFILE"
-	fi
-	[ -f "$HOME/.dotfiles/runcom/$DOTFILE" ] && ln -sf "$HOME/.dotfiles/runcom/$DOTFILE" "$HOME/$DOTFILE"
-done
-
-# Symlink .config directories
-for CONFIG in `find $HOME/.dotfiles/config -type d -printf "%f\n"`
-do
-	echo "Creating symlink to $CONFIG (config) in home directory."
-	if [ -d "$HOME/.config/$CONFIG" ]; then
-		[[ -d "$HOME/.dotfiles/backup/config" ]] || mkdir -p "$HOME/.dotfiles/backup/config"
-		echo "$CONFIG already exists in the home directory, creating backup at $HOME/.dotfiles/backup/$CONFIG"
-		cp -r "$HOME/.config/$CONFIG" "$HOME/.dotfiles/backup/$CONFIG"
-		rm -r "$HOME/.config/$CONFIG"
-	fi
-	[ -d "$HOME/.dotfiles/config/$CONFIG" ] && ln -sf "$HOME/.dotfiles/config/$CONFIG" "$HOME/.config/$CONFIG"
-done
-
-# Symlink .config files
-for CONFIG in `find $HOME/.dotfiles/config -type f -printf "%f\n"`
-do
-	echo "Creating symlink to $CONFIG (config) in home directory."
-	if [ -f "$HOME/.config/$CONFIG" ]; then
-		[[ -d "$HOME/.dotfiles/backup/config" ]] || mkdir -p "$HOME/.dotfiles/backup/config"
-		echo "$CONFIG already exists in the home directory, creating backup at $HOME/.dotfiles/backup/$CONFIG"
-		cp "$HOME/.config/$CONFIG" "$HOME/.dotfiles/backup/$CONFIG"
-	fi
-	[ -f "$HOME/.dotfiles/config/$CONFIG" ] && ln -sf "$HOME/.dotfiles/config/$CONFIG" "$HOME/.config/$CONFIG"
-done
-
-# https://www.gnu.org/gnu/linux-and-gnu.en.html
-
-# Exporting PATH
-export PATH;
-
-# Completion Message & Information
-echo "→ Installation has been completed!"
-echo "→ Thank you for using RealStr1ke's dotfiles, inspired from many dotfile repos."
-echo "→ For extra custom configurations, you can edit the ~/.dotfiles/system/.extra file (untracked)."
-echo "→ Next up, you should run '~/.dotfiles/packages/install.sh' to install saved packages."
-echo "→ Remember to restart your bash prompt after completing the installation."
-echo "→ Enjoy!"
