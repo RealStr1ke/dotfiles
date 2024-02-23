55644e948d7a8125d55979d8cd2610564360b7c5
Thandi M. <TMenelas@outlook.com>
2024-02-23 12:46:59 -0500

    Added prettier config

---
+++ bootstrap/os/linux/runcom/.prettierrc.js
@@ -0,0 +1,15 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    trailingComma: "all",
+    semi: true,
+    singleQuote: true,
+    bracketSameLine: true,
+    useTabs: true,
+    
+};
+
+module.exports = config;
+
